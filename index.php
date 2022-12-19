ff3a90c3ecd3824295cd57bdeedfff5ae9c44e53
Vaishnavi-Waghmale <vaishnaviwaghmale@gmail.com>
2022-12-19 12:42:13 +0530

    Upload all files

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="login-box">
+    <h2>Login As</h2>
+<form action="signinadmin.php" method="post">
+<input class="button" type="submit"
+                     name="login" value="Admin login">
+            
+    </form>
+    <form action="signinuser.php" method="post">
+<input class="button" type="submit"
+                     name="login" value="User login">
+            
+    </form>
+</div>
+    
+</body>
+</html>
---
+++ userpage.php
@@ -0,0 +1,3 @@
+<h1>Welcom to this site.
+    Hello User..
+</h1>
